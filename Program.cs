52a1c9ed93809e603f4aabd0beef29ddd24120ce
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2022-04-24 19:39:38 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Linq;
+ 
+class Program
+{
+    static double[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+ 
+    static (double a, double b) Read2L()
+    {
+        var a = ReadL();
+        return (a[0], a[1]);
+    }
+
+    static (double a, double b, double c) Read3L()
+    {
+        var a = ReadL();
+        return (a[0], a[1], a[2]);
+    }
+ 
+    /// 階乗を求めるメソッド
+    static int Factorial(int n)
+    {
+        if (n == 0) return 1;
+        return n * Factorial(n - 1);
+    }
+ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hoge");
+    }
+}
